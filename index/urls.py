753c00af857b2ae67f9df398556d51949859d86d
Vanoss_g98 <amirsaman.shohrati@gmail.com>
2024-11-09 13:55:09 +0330

    added lots of content

--- index/urls.py
+++ index/urls.py
@@ -6,6 +6,12 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/', ContactView.as_view(), name='contact'),
-    path('portfolio/', PortfolioListView.as_view(), name='portfolio-list'),
+    path('portfolio/logo-design', LogoArchive.as_view(), name='logo-design-archive'),
+    path('portfolio/visual-brand-identity', DigitalArchive.as_view(), name='digital-branding'),
+    path('portfolio/motion-design', MotionDesignArchive.as_view(), name='motion-design'),
+    path('portfolio/advertising-photography', AdvertisingPhotoArchive.as_view(), name='advertising-photography'),
+    path('portfolio/illustration', IllustrationArchive.as_view(), name='illustration'),
+    path('portfolio/gamification', GameDesignArchive.as_view(), name='gamification'),
+    path('portfolio/video-making', VideoMakingArchive.as_view(), name='video-making'),
     path('portfolio/detail', PortfolioDetailSample.as_view(), name='portfolio-detail'),
 ]
